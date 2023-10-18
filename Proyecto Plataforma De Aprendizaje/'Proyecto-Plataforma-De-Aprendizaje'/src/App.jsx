93241e580776274a5172a76ae2d5d1ba2bc2bd63
Jonathan Jaramillo <125410451+JonathanJara01@users.noreply.github.com>
2023-10-18 15:25:15 -0500

    el primer comit
    todos los archivos

---
+++ Proyecto Plataforma De Aprendizaje/'Proyecto-Plataforma-De-Aprendizaje'/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import imagenLogo from './Components/Images/Captura_de_pantalla_2023-09-13_151842-removebg-preview.png'
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Nombre del proyecto</h1>
+      <img src={imagenLogo} alt="imagen_logo" />
+    </div>
+    <div>
+      <h2>Aca va el navbar</h2>
+    </div>
+    <div>Aca van las promociones</div>
+    <div>
+      <h3>aca va el contenido</h3>
+      <p>soy la carta</p>
+      <span>titulos x</span>
+    </div>
+    </>
+  )
+}
+
+export default App
