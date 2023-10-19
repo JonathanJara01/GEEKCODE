d6b212984230bbccb8d74a893382fd25dab37de2
Jonathan Jaramillo <125410451+JonathanJara01@users.noreply.github.com>
2023-10-19 01:18:10 -0500

    Commit 4
    Subo footer terminado, junto con el home adicionándole el código bootstrap

--- Proyecto Plataforma De Aprendizaje/'Proyecto-Plataforma-De-Aprendizaje'/src/Components/Footer.jsx
+++ Proyecto Plataforma De Aprendizaje/'Proyecto-Plataforma-De-Aprendizaje'/src/Components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import LogoFooter from './Images/Captura_de_pantalla_2023-09-13_151842-removebg-preview.png'
 
 const Footer = () => {
   return (
-    <div>
-    <div className="row">
-            <footer className="py-3 my-4">
+    <div className="container my-3">
+    <div className="row align-items-center animate__animated animate__rubberBand">
+            <footer className="py-3 my-4 col-sm-12 col-md-6 col-lg-6 col-xl-12">
+            <div className="col-sm-12 col-md-9 col-lg-9 col-xl-3 mt-3">
+            <img src={LogoFooter} className="img-fluid" alt="imagenlogofooter" />
+            </div>
                 <ul className="nav justify-content-center">
                     <li className="nav-item"><a href="#!" className="nav-link">Acerca De Nosotros</a></li>
-                    <li className="nav-item"><a href="#!" className="nav-link">Redes</a></li>
+                    <li className="nav-item"><a href="#!" className="nav-link">Facebook</a></li>
+                    <li className="nav-item"><a href="#!" className="nav-link">Instagram</a></li>
+                    <li className="nav-item"><a href="#!" className="nav-link">Whatsapp</a></li>
                 </ul>
                 <p className="text-center text-muted">© 2023 Jonathan Jaramillo Zapata & Alexander Emanuel Bris</p>
             </footer>
--- Proyecto Plataforma De Aprendizaje/'Proyecto-Plataforma-De-Aprendizaje'/src/Components/Home.jsx
+++ Proyecto Plataforma De Aprendizaje/'Proyecto-Plataforma-De-Aprendizaje'/src/Components/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1>Home</h1>
+        <div className="row animate__animated animate__rubberBand">
+            <h1 className="display-3">GEEKHOME</h1>
+            <p className="text-justify">ACA ESTAN TODOS LOS CURSOS GEEK</p>
+        </div>
     </div>
   )
 }
